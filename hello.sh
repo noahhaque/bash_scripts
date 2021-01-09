501ecf4d8099be0d3d8680a619a453389e6f93ae
asimzafar202 <54381665+asimzafar202@users.noreply.github.com>
2021-01-08 19:51:03 -0500

    Development (#1)
    * added linting ci/cd workflow
    *Erased previous contents to add a welcoming script in hello.sh
    Co-authored-by: Noah Haque <noah@noahs-mbp.myfiosgateway.com>
    Co-authored-by: Asim Zafar <Asim@Users-MacBook-Pro.local>
    Co-authored-by: Noizebomb <73506881+Noizebomb@users.noreply.github.com>

--- hello.sh
+++ hello.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+greeting="welcome back"
+user=$(whoami)
+day=$(date +%A)
+
+echo "$greeting $user! Today is $day, which is the best day of the week!"
+echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
