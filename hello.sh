25cf7fd0b36b5915d1eb805b3f9e4a9308ec4cea
Noizebomb <73506881+Noizebomb@users.noreply.github.com>
2021-01-08 19:32:40 -0500

    corrected greeting variable

--- hello.sh
+++ hello.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-greeting="welcome"
-user=(whoami)
+greeting="welcome back"
+user=$(whoami)
 day=$(date +%A)
 
-echo "greeting back $user! Today is $day, which is the best day of the week!"
+echo "$greeting $user! Today is $day, which is the best day of the week!"
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
